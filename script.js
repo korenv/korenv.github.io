bbb99f36975d3bebadaae719fa8889134951a4d1
korenv <93277074+korenv@users.noreply.github.com>
2021-11-17 18:31:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("submit").onclick = function passwordfunc(){
     var myInput = document.getElementById("psw").value;
     var password = "password";
     if (myInput == password) {
-      location.replace("links.html")}
+      location.replace("password/is/correct/here/are/the/links")}
     else {
       location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ%22");
     }}
