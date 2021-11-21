57d5a74315363f365699883cb3b467e5fd63bb23
korenv <93277074+korenv@users.noreply.github.com>
2021-11-21 16:34:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-document.getElementById("submit").onclick = function passwordfunc(){
-    var myInput = document.getElementById("psw").value;
-    var password = "password";
-    if (myInput == password) {
-      location.replace("dflkgjdfjgmjkdfngmldfknmhlkdfmngolsfjoamflsemegolsmgksdmgmdfkgmnsdjkvkfbnsdjgklndjkgndrkgnkds")}
-    else {
-      window.open("https://www.youtube.com/watch?v=9K2Y-rfUy_4");
-    }}
+document.getElementById("submit").onclick = function passwordfunc()
+{
+  var myInput = document.getElementById("psw").value;
+  var password = "password";
+  if (myInput == password)
+  {
+    location.replace("dflkgjdfjgmjkdfngmldfknmhlkdfmngolsfjoamflsemegolsmgksdmgmdfkgmnsdjkvkfbnsdjgklndjkgndrkgnkds")
+  }
+  else 
+  {
+    random = Math.floor(Math.random() * 2);
+    if(random == 1) {window.open("https://www.youtube.com/watch?v=9K2Y-rfUy_4");}
+    else if(random == 0) {document.getElementById("demo").innerHTML = "טעות"}
+  }
+}
